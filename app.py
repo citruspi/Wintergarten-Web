43a9303fdd2c8e497e993f44e76f6dad57c4e703
Mihir Singh <me@mihirsingh.com>
2013-12-17 06:23:37 -0500

    Split template into different files

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ app = Flask(__name__)
 app.config.from_object('config.Development')
 
 @app.route("/")
-def hello():
+def search():
 
-    return render_template("main.html")
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run()
