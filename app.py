58d6cc8852e4ef36f99562e53640390ac8cfd201
Mihir Singh <me@mihirsingh.com>
2013-12-18 04:07:46 -0500

    Consolidated special listings into a single route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 import requests
 import os
 from models import film
@@ -9,48 +9,7 @@ app.config.from_object('config.Development')
 @app.route("/")
 def home():
 
-    return render_template("home.html")
-
-@app.route('/popular', methods=['GET'])
-def popular():
-
-	response = requests.get('http://api.themoviedb.org/3/movie/popular', params={'api_key':os.environ['TMDB_API_KEY']}).json()
-
-	films = []
-
-	for result in response['results']:
-
-		films.append(film(result['id'], app.config['IMG_BASE_URL']))
-
-	return	render_template("listing.html", films=films)
-
-@app.route('/theatres', methods=['GET'])
-def theatres():
-
-	response = requests.get('http://api.themoviedb.org/3/movie/now_playing', params={'api_key':os.environ['TMDB_API_KEY']}).json()
-
-	films = []
-
-	for result in response['results']:
-
-		further = requests.get('http://api.themoviedb.org/3/movie/'+str(result['id']), params={'api_key':os.environ['TMDB_API_KEY']}).json()
-
-		films.append(film(result['id'], app.config['IMG_BASE_URL']))		
-
-	return	render_template("listing.html", films=films)	
-
-@app.route('/upcoming', methods=['GET'])
-def upcoming():
-
-	response = requests.get('http://api.themoviedb.org/3/movie/upcoming', params={'api_key':os.environ['TMDB_API_KEY']}).json()
-
-	films = []
-
-	for result in response['results']:
-
-		films.append(film(result['id'], app.config['IMG_BASE_URL']))
-
-	return	render_template("listing.html", films=films)		
+    return render_template("home.html")	
 
 @app.route('/search', methods=['POST', 'GET'])
 def search():
@@ -64,6 +23,33 @@ def search():
 		films.append(film(result['id'], app.config['IMG_BASE_URL']))
 
 	return	render_template("search.html", query=request.form['query'], films=films)
+
+@app.route('/<listing>', methods=['GET'])
+def listing(listing):
+
+	map = {
+
+		'popular': 'popular',
+		'theatres': 'now_playing',
+		'upcoming': 'upcoming'
+
+	}
+
+	if listing in map:
+
+		response = requests.get('http://api.themoviedb.org/3/movie/' + map[listing], params={'api_key':os.environ['TMDB_API_KEY']}).json()
+
+		films = []
+
+		for result in response['results']:
+
+			films.append(film(result['id'], app.config['IMG_BASE_URL']))
+
+		return	render_template("listing.html", films=films)	
+
+	else:
+
+		abort(404)
 
 if __name__ == "__main__":
 
