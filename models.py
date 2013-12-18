b91f813b83ae56a16a202df1c84c10e7252bc9f9
Mihir Singh <me@mihirsingh.com>
2013-12-17 22:19:11 -0500

    Added Film model

---
+++ models.py
@@ -0,0 +1,14 @@
+class film (object):
+
+	def __init__(self, adult, backdrop_path, id, original_title, release_date,
+				 poster_path, title, imdb_poster):
+
+		self.adult = adult
+		self.backdrop_path = backdrop_path
+		self.id = id
+		self.original_title = original_title
+		self.release_date = release_date
+		self.poster_path = poster_path
+		self.title = title
+		self.imdb_poster = imdb_poster
+
