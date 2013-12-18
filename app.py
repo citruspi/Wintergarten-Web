6064921d68229c7a91101e1738caa8b1f18c303c
Mihir Singh <me@mihirsingh.com>
2013-12-17 22:18:58 -0500

    Return poster images on search

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import requests
+import os
+from models import film
 
 app = Flask(__name__)
 app.config.from_object('config.Development')
@@ -11,7 +14,31 @@ def home():
 @app.route('/search', methods=['POST', 'GET'])
 def search():
 
-	return	render_template("search.html", query=request.form['query'])
+	response = requests.get('http://api.themoviedb.org/3/search/movie', params={'api_key':os.environ['TMDB_API_KEY'], 'query':request.form['query']}).json()
+
+	films = []
+
+	for result in response['results']:
+
+		further = requests.get('http://api.themoviedb.org/3/movie/'+str(result['id']), params={'api_key':os.environ['TMDB_API_KEY']}).json()
+
+		print further
+
+		imdb = requests.get('http://www.omdbapi.com/', params={'i':further['imdb_id']}).json()
+
+		print imdb
+
+		films.append(film(result['adult'],
+						  result['backdrop_path'],
+						  result['id'],
+						  result['original_title'],
+						  result['release_date'],
+						  result['poster_path'],
+						  result['title'],
+						  imdb['Poster']))
+
+	return	render_template("search.html", query=request.form['query'], films=films)
 
 if __name__ == "__main__":
-    app.run()
+
+	app.run()
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Config(object):
 
   DEBUG = False
+  IMG_BASE_URL = 'https://image.tmdb.org/t/p'
 
 class Development(Config):
 
